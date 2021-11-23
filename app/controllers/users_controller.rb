d42c7d785ef9efef5259fce1dfa66a56b848322f
meghanayak <meganayak@gmail.com>
2021-11-23 10:06:26 +0530

    Corrected the routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,33 +2,33 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-    def index
-      render plain: User.all.to_a
-    end
+  def index
+    render plain: User.all.to_a
+  end
 
-    def login
-      email = params[:email]
-      password = params[:password]
-      user = User.find_by(email: email, password: password)
-      render plain: user.present?
-    end
+  def login
+    email = params[:email]
+    password = params[:password]
+    user = User.find_by(email: email, password: password)
+    render plain: user.present?
+  end
 
-    def show
-      id = params[:id]
-      user = User.find(id)
-      render plain: user.to_pleasant_string
-    end
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_pleasant_string
+  end
 
-    def create
-      name_new=params[:name]
-      email_new=params[:email]
-      password_new=params[:password]
-      new_user=User.create!(
-      name:name_new,
-      email:email_new,
-      password:password_new,
-        )
-      response_text= "Hey ,your new user is created with the Name: #{new_user.name}"
-      render plain: response_text
-    end  
+  def create
+    name_new = params[:name]
+    email_new = params[:email]
+    password_new = params[:password]
+    new_user = User.create!(
+      name: name_new,
+      email: email_new,
+      password: password_new,
+    )
+    response_text = "Hey ,your new user is created with the Name: #{new_user.name}"
+    render plain: response_text
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
-  post "/users/login/", to: "users#login"
+  resources :todos
   resources :users
+  post "users/login", to: "users#login"
 end
